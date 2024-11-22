0ef411ee869aa744c5505af0c5086fa820319b24
LuckyMooDevTeam <moomincoinsol@gmail.com>
2024-11-22 21:16:45 +0800

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,11 +1,11 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "1reG1xxGsoA0UKVBTnBFihdeq";
-const APP_SECRET = "PvlMl5lPONTX0V2t8yvDs84IWnwFBHuP0NWB6ZHhb9tJGJNPSL";
-const ACCESS_TOKEN = "1858875279315472384-NcfRVB1Djw3PRf2y0jBcYmnOYSW7z6";
+const APP_KEY = "Db0r35RBzuqrrCPWKOQtCkhFj";
+const APP_SECRET = "MOIeseyzWqwyEh73pLNpTClkXiVJgsRhuMxI4a4bBKEMF2icGn";
+const ACCESS_TOKEN = "1769254672882155520-jvu9tB0fOQZhVy7ZFaxxlULqe8BU1r";
 const ACCESS_SECRET = "ffAZYF1eGBgJnVUwkB2v8VJlRhQJQK0B0VfhUytdDroBQ";
-const GEMINI_API_KEY = "AIzaSyBosk7fz-esFFtB5whha46MYqtUObVFsb0";
+const GEMINI_API_KEY = "ELJZZDl4o2lwFgmBOtQkYfzZsuJm2ScGRlPMFGlODZgqM";
 
 const SECRETS = {
   APP_KEY,
