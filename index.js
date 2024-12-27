dcf174980765e0f621ca2352946b1af334b9c9cf
LuckyMooDevTeam <moomincoinsol@gmail.com>
2024-12-27 17:45:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a tweet saying there a new AI breakthrough recently, launching new development very soon and stay tune, asking the people to join the $LuckyMoo herd & pump together to make it the 1st cow-themed coin to hit $1B Market Cap, breaking all time highs after all time highs, promote either Dextools: https://www.dextools.io/app/en/solana/pair-explorer/5mnKKcKWKvW8Ejr8qFLkuEXr9AN9Gm1YeLvufNjfZe4u?t=1732281338179 or Birdeyes: https://birdeye.so/token/CbbCD8B43sGxCUCcNTuy8ASCAkhVkuveVwraGQC11Hmf/5mnKKcKWKvW8Ejr8qFLkuEXr9AN9Gm1YeLvufNjfZe4u?chain=solana or join $LUCKYMOO telegram group: https://t.me/LUCKYMOOCOIN or visit Luckymoo website at https://luckymoosol.com/, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis and crypto and meme related hashtags";
+    "generate a tweet saying we have successfully developed an AI Agent and just applied for listing in 4 new major platforms and paid and approved for priortised listing, asking the people to join the $LuckyMoo herd & pump together to make it the 1st cow-themed coin to hit $1B Market Cap, breaking all time highs after all time highs, promote either Dextools: https://www.dextools.io/app/en/solana/pair-explorer/5mnKKcKWKvW8Ejr8qFLkuEXr9AN9Gm1YeLvufNjfZe4u?t=1732281338179 or join $LUCKYMOO telegram group: https://t.me/LUCKYMOOCOIN or visit Luckymoo website at https://luckymoosol.com/, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis and crypto and meme related hashtags";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
